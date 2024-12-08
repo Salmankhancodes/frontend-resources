26d8ebe4596b74a5faf2acbcca29abb985271ca9
Salmankhancodes <117salmankhan@gmail.com>
2024-12-09 02:03:33 +0530

    Added functionality to open current card image in carousel

--- Frontend Learning Resources/FE Practise/Machine coding/modal/script.js
+++ Frontend Learning Resources/FE Practise/Machine coding/modal/script.js
@@ -9,6 +9,69 @@ document.addEventListener('DOMContentLoaded', () => {
     'https://fastly.picsum.photos/id/27/3264/1836.jpg?hmac=p3BVIgKKQpHhfGRRCbsi2MCAzw8mWBCayBsKxxtWO8g',
     'https://fastly.picsum.photos/id/29/4000/2670.jpg?hmac=rCbRAl24FzrSzwlR5tL-Aqzyu5tX_PA95VJtnUXegGU',
   ]
+  const createCarousel = (imagesArray) => {
+    // create carousel frame
+    const carouselContainer = document.querySelector('.carousel-container')
+    const frame = document.createElement('div')
+    const slideIndicators = document.createElement('div')
+    slideIndicators.setAttribute('class', 'slide-indicator-container')
+    frame.setAttribute('class', 'frame')
+    // adding sliders to frame
+    imagesArray.forEach((image, index) => {
+      const imgContainer = document.createElement('img')
+      imgContainer.setAttribute('class', 'slider')
+      imgContainer.setAttribute('src', image)
+      // put sliders in consecutive position
+      imgContainer.style.left = `${index * 100}%`
+      frame.appendChild(imgContainer)
+
+      const indicator = document.createElement('div')
+      indicator.setAttribute('class', 'indicator')
+      slideIndicators.appendChild(indicator)
+    })
+    slideIndicators.firstChild?.classList?.add('active')
+    carouselContainer.appendChild(frame)
+    carouselContainer.appendChild(slideIndicators)
+    return carouselContainer
+  }
+  const createButton = (buttonName, className) => {
+    const btn = document.createElement('button')
+    btn.setAttribute('class', `btn ${className}`)
+    btn.innerText = buttonName
+    return btn
+  }
+  const updateCarousel = (activeSlide) => {
+    const allImages = document.querySelectorAll('.slider')
+    allImages.forEach((image) => {
+      image.style.transform = `translate(-${activeSlide * 100}%)`
+    })
+    const allIndicators = document.querySelectorAll('.indicator')
+    allIndicators.forEach((indicator, index) => {
+      index === activeSlide
+        ? indicator.classList.add('active')
+        : indicator.classList.remove('active')
+    })
+  }
+  function carouselComponent(imagesData, activeSlide) {
+    const carouselContainer = createCarousel(imagesData)
+    const prevBtn = createButton('< PREV', 'prev-btn')
+    const nextBtn = createButton('NEXT >', 'next-btn')
+
+    prevBtn.addEventListener('click', () => {
+      activeSlide--
+      activeSlide = activeSlide < 0 ? imagesData.length - 1 : activeSlide
+      updateCarousel(activeSlide)
+    })
+
+    nextBtn.addEventListener('click', () => {
+      activeSlide++
+      activeSlide = activeSlide === imagesData.length ? 0 : activeSlide
+      updateCarousel(activeSlide)
+    })
+    carouselContainer.querySelector('.frame').appendChild(prevBtn)
+    carouselContainer.querySelector('.frame').appendChild(nextBtn)
+    updateCarousel(activeSlide)
+  }
   // display on UI as cards
   const updateUI = function () {
     const productsContainer = document.createElement('div')
@@ -23,6 +86,7 @@ document.addEventListener('DOMContentLoaded', () => {
       card.addEventListener('click', () => {
         const modal = document.querySelector('.modal-base')
         modal.style.display = 'block'
+        carouselComponent(imagesData, idx)
       })
       productsContainer.appendChild(card)
     })
@@ -42,17 +106,27 @@ document.addEventListener('DOMContentLoaded', () => {
       console.log("can't fetch data", error)
     }
   }
+  const removecaro = () => {
+    const doc = document.querySelector('.carousel-container')
+    doc.innerHTML = ''
+    // doc.forEach((ele) => ele.remove())
+    // console.log(doc)
+  }
 
   const createModalUI = function () {
     const modalBase = document.createElement('div')
     const modalContent = document.createElement('div')
+    const carouselContainer = document.createElement('div')
+    carouselContainer.setAttribute('class', 'carousel-container')
     const closeBtn = document.createElement('span')
     closeBtn.setAttribute('class', 'modal-close-button')
     closeBtn.innerText = 'x'
     closeBtn.addEventListener('click', () => {
       modalBase.style.display = 'none'
+      removecaro()
     })
     modalContent.setAttribute('class', 'modal-content')
+    modalContent.appendChild(carouselContainer)
     modalBase.appendChild(closeBtn)
     modalBase.appendChild(modalContent)
     modalBase.setAttribute('class', 'modal-base')
